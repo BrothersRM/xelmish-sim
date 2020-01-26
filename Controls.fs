8f714702445f0a82fb3ec694bfa149596589142f
Ryan Brothers <BrothersRM1@gmail.com>
2020-01-26 01:10:54 -0600

    added gravity and onclick creation to boxes

---
+++ Controls.fs
@@ -0,0 +1,13 @@
+﻿module xelmish_template.Controls
+open Xelmish.Viewables
+
+let text = text "defaultFont"
+
+let button displaytext click position size backcolor forecolour =
+    let x, y = position
+    let width, height = size
+    [
+        colour backcolor (width, height) (x, y)
+        text 20. forecolour (-0.5, -0.5) displaytext (x + width/2, y+height/2)
+        onclick click (width, height) (x, y)
+    ]   
--- Program.fs
+++ Program.fs
@@ -1,46 +1,54 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open Elmish
+﻿open Elmish
 open Xelmish.Model
 open Xelmish.Viewables
+open xelmish_template.Controls
 
 type Model = {
-    counter: int
+    boxes: (int*int) list
     tickCount: int64
 }
 
+
 let init () = {
-    counter = 0
     tickCount = 0L
+    boxes = [(10, 10); (100, 100)]
 }
 
 type Message =
-    | Increment of amount: int
-    | Decrement of amount: int
     | Tick of int64
-      
+    | AddBox of int*int
+    
+let tryMoveDown (position: int*int) (stepSize: int) =
+    match position with
+    | (x, y) when y < 600 - stepSize -> (x, y+stepSize)
+    | _ -> position
+    
 let update message model =
     match message with
-    | Increment x -> {model with counter = model.counter + x;}  
-    | Decrement x -> {model with counter = model.counter - x;}
-    | Tick x -> {model with tickCount = x} 
-    
+    | Tick x -> {model with tickCount = x; boxes = model.boxes |> List.map (fun x -> tryMoveDown x 10)} 
+    | AddBox (x, y) -> {model with boxes = (List.append model.boxes [(x, y)]) }    
+
 let view model dispatch =
     [
-        yield text "defaultFont" 20. Colour.White (0., 0.) (sprintf "%i" model.counter) (300, 300)
+        yield! [0 .. 10 .. 600] |> Seq.map (fun x -> colour Colour.DarkGray (1, 600) (x, 0))
+        yield! [0 .. 10 .. 600] |> Seq.map (fun y -> colour Colour.DarkGray (600, 1) (0, y))
+        yield onclickpoint (fun inputs ->
+            let x, y = inputs
+            dispatch (AddBox (x - x%10, y - y%10))            
+        )
         yield onupdate (fun inputs ->
-            let interval = 1000L
+            let interval = 100L
             if(inputs.totalGameTime - model.tickCount) >= interval then
                 dispatch (Tick inputs.totalGameTime)
-                dispatch (Increment 1)
             )
+        yield! model.boxes |> Seq.map (fun x -> colour Colour.Black (10, 10) x )
     ]
 
 [<EntryPoint>]
 let main _ =
     let config = {
         resolution = Windowed (600, 600)
-        clearColour = Some Colour.Black
+        clearColour = Some Colour.SlateGray
         mouseVisible = true
         assetsToLoad = [
             PipelineFont ("defaultFont", "./content/SourceCodePro")
